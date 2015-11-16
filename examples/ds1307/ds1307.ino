3850eed316fa09e7897c3a2cebbbcf7d51dd0527
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2015-11-16 13:01:28 -0500

    Update ds1307.ino

--- examples/ds1307/ds1307.ino
+++ examples/ds1307/ds1307.ino
@@ -1,10 +1,14 @@
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
-#include <Wire.h>
-#include "RTClib.h"
+#include <RTClib_Tiny.h>
 
-#if defined(ARDUINO_ARCH_SAMD)  // for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
-   #define Serial SerialUSB
-#endif
+//#include <Wire.h>
+//#include <TinyWireM.h>
+#include <SoftI2CMaster.h> 
+//comment out all but one of the above library includes. 
+
+//if using SoftI2CMaster (ie, on tiny841 or 828), uncomment this, otherwise comment out. 
+SoftI2CMaster softi2c=SoftI2CMaster( 7, 2, 0 ); //sclPin=7, sdaPin=2
+
 
 RTC_DS1307 rtc;
 
